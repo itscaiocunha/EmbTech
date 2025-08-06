2276463f6c2e05fb60055871a199502fb1fcb7fa
itscaiocunha <caio.cunha@sou.unifeob.edu.br>
2025-08-06 15:37:39 -0300

    feat: alteração de solucoes para serviços

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -60,7 +60,7 @@ const Navigation = () => {
                 Produtos
               </a>
               <a href="/solucoes" className="text-foreground hover:text-primary transition-colors">
-                Soluções
+                Serviços
               </a>
               <a href="/blog" className="text-foreground hover:text-primary transition-colors">
                 Blog
